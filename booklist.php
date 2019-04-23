f06f4656223f627079adceee20c121cbcd425c6d
Brittany Ohalete <bopho@Brittanys-MacBook-Pro.local>
2019-04-23 19:33:03 -0400

    Reading valyes

--- booklist.php
+++ booklist.php
@@ -4,7 +4,12 @@
 // getting data from database
 $conn = new mysqli("localhost", "root", "521347", "books");
 // Select database
-$result = mysqli_query($conn, "SELECT * FROM Titles");
+$result = mysqli_query($conn, "SELECT DISTINCT book.title, a.F_Name, a.L_Name, y.bookYear, book.categoryID, c.CategoryName, p.price
+                                FROM Titles book, Years y,Author a,Prices p,Category c
+                                WHERE book.categoryID = c.categoryID
+                                AND a.bookID = book.bookID
+                                AND y.bookID = book.bookID
+                                AND p.bookID = book.bookID");
 
 //storing in array
 $data = array();
