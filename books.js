a7d5e3cde95ae9a135d637eb6eb22bb6a366a36d
Brittany Ohalete <bopho@Brittanys-MacBook-Pro.local>
2019-04-23 21:23:48 -0400

    List books button and structure data

--- books.js
+++ books.js
@@ -3,11 +3,15 @@ var ajax = new XMLHttpRequest();
 var method = "GET";
 var url = "booklist.php";
 var asynchronous = true;
+var genre = [];
 
 ajax.open(method, url, asynchronous);
 // sendindg ajax request
 ajax.send();
 
+document.getElementById("list-books").addEventListener("click", function(){
+  document.getElementById("book-data").style.display = 'block';
+});
 // receiving response from booklist.php
 ajax.onreadystatechange = function()
 {
@@ -16,5 +20,44 @@ ajax.onreadystatechange = function()
     // converting JSON back to array
     var data = JSON.parse(this.responseText);
     console.log(data); // for debugging
+
+    var input = "";
+    var html = "";
+
+    // category buttons
+    for (var i =0; i<data.length; i++){
+      var cat = data[i].CategoryName;
+      if(!genre.includes(cat)){
+          genre.push(cat);
+          input += "<input type='radio'>"+ cat + "</input>";
+        }
+    }
+
+    document.getElementById("radio-buttons").innerHTML = input;
+
+
+    //loop through books
+    for (var i =0; i<data.length; i++){
+      var title = data[i].title;
+      var F_Name = data[i].F_Name;
+      var L_Name = data[i].L_Name;
+      var bookYear = data[i].bookYear;
+      var categoryID = data[i].categoryID;
+      var CategoryName = data[i].CategoryName;
+      var price = data[i].price;
+
+      //append as html
+      html += "<tr class='book-details'>";
+        html += "<td id='book-title'>" + title + "</td>";
+        html += "<td class='book-author'>" + F_Name + "</td>";
+        html += "<td class='book-author'>" + L_Name + "</td>";
+        html += "<td id='book-Year'>" + bookYear + "</td>";
+        html += "<td id='book-categoryID'>" + categoryID + "</td>";
+        html += "<td id='book-categoryName'>" + CategoryName + "</td>";
+        html += "<td id='book-price'>"+ price + "</td>";
+      html += "</tr>";
+    }
+
+    document.getElementById("data").innerHTML = html;
   }
 }
--- books.php
+++ books.php
@@ -2,9 +2,26 @@
 
 ?>
 <head>
+  <link rel="stylesheet" href="books.css">
   <title>Book List</title>
 </head>
 <body>
   <h1>Book List</h1>
+  <div id="radio-buttons" class="btns"></div>
+  <button class="btns" id="list-books">List Books</button>
+  <table id="book-data">
+    <tr>
+      <th>Title</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Year</th>
+      <th>categoryID</th>
+      <th>Category</th>
+      <th>Price</th>
+    </tr>
+    <tbody id="data">
+    </tbody>
+  </table>
 </body>
+
 <script src="books.js" type="text/javascript"></script>
